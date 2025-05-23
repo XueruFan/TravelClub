016b978a3c29f7d1c3124982a00a91f7e1acaa20
Xue-Ru Fan <xueru@mail.bnu.edu.cn>
2025-05-23 22:45:13 +0800

    update from mac

--- code/MSHBM/TravelClub.m
+++ code/MSHBM/TravelClub.m
@@ -10,19 +10,19 @@ clear;clc
 % delete(gcp('nocreate'));
 % parpool('Processes', 56, 'IdleTimeout', Inf);
 
-%—————————— MAC
+%—————————— For Xueru's MAC
 setenv('CBIG_CODE_DIR', '/Users/xuerufan/matlab-toolbox/CBIG');
 setenv('FREESURFER_HOME', '/Applications/freesurfer');
-project_dir = '/Volumes/Zuolab_Xueru/TravelClub';
-code_dir = '/Volumes/Zuolab_Xueru/TravelClub/code/MSHBM'; 
-temp_dir = '/Volumes/Zuolab_Xueru/TravelClub/templet';
+project_dir = '/Volumes/FanXueru/TravelClub';
+code_dir = '/Users/xuerufan/TravelClub/code/MSHBM'; 
+temp_dir = '/Users/xuerufan/TravelClub/templet';
 
 addpath('/Applications/freesurfer');
 addpath('/Applications/workbench/bin_macosx64/');
-addpath(genpath('/home/ubuntu/homes/FanXueru/toolbox-matlab/CBIG'));
-addpath(genpath('/media/ubuntu/Zuolab_Xueru/TravelClub'));
+addpath(genpath('/Users/xuerufan/matlab-toolbox/CBIG'));
+addpath(genpath('/Volumes/FanXueru/TravelClub'));
 
-%—————————— Linux
+%—————————— For Linux @205
 % setenv('CBIG_CODE_DIR', '/home/ubuntu/homes/FanXueru/toolbox-matlab/CBIG');
 % setenv('FREESURFER_HOME', '/home/ubuntu/Softwares/FREESURFER/freesurfer');
 % project_dir = '/media/ubuntu/Zuolab_Xueru/TravelClub';
@@ -58,6 +58,7 @@ cd(code_dir)
 disp('Step 0 DONE!');
 
 %% Step 1 prepare the fMRI list files
+% 生成站点-被试的fMRI数据list的txt文件
 
 dataFolder = fullfile(project_dir, 'data');
 outputFolder = fullfile(project_dir, 'output', 'generate_profiles_and_ini_params', 'data_list', 'fMRI_list');
@@ -76,64 +77,62 @@ siteFolders = siteFolders(~ismember({siteFolders.name}, {'.', '..'}));
 % end
 
 
+% %---- 并行处理使用下面这部分代码
+% flatIndices = [];
+% for i = 1:nsite
+%     for j = 1:nsub
+%         flatIndices = [flatIndices; i, j];
+%     end
+% end
+% 
+% parfor idx = 1:size(flatIndices, 1)
+% 
+%     i = flatIndices(idx, 1); % 站点索引
+%     j = flatIndices(idx, 2); % 被试索引
+% 
+%     try
+%         siteFolderName = siteFolders(i).name;
+%         siteLetter = siteFolderName(end);
+%         siteFolderPath = fullfile(dataFolder, siteFolderName);
+%         
+%         subFolderName = subs{j};
+%         subfolderPath = fullfile(siteFolderPath, subFolderName);
+%                 
+%         if exist(subfolderPath, 'dir')
+%             niiFilePaths = {};
+%             runFolders = dir(fullfile(subfolderPath, '*PA_run*'));
+% 
+%             for k = 1:length(runFolders)
+%                 runFolderName = runFolders(k).name;
+%                 runFolderPath = fullfile(subfolderPath, runFolderName);
+%                 niiFile = dir(fullfile(runFolderPath, '*.dtseries.nii'));
+% 
+%                 if isempty(niiFile)
+%                     error('This sub doesnt have this data. Please check!');
+%                 end
+% 
+%                 niiFilePaths{end+1} = fullfile(runFolderPath, niiFile(1).name);
+%             end
+% 
+%             if ~isempty(niiFilePaths)
+%                 txtFileName = fullfile(outputFolder, sprintf('%s_site-%s.txt', subFolderName, siteLetter));
+%                 fileID = fopen(txtFileName, 'w');
+%                 for n = 1:length(niiFilePaths)
+%                     if n == length(niiFilePaths)
+%                         fprintf(fileID, '%s', niiFilePaths{n});
+%                     else
+%                         fprintf(fileID, '%s ', niiFilePaths{n});
+%                     end
+%                 end
+%                 fclose(fileID);
+%             end
+%         end
+%     catch ME
+%         fprintf('Error processing subject %s in site %s: %s\n', siteFolderName, ME.message);
+%     end
+% end
 
-%---- 并行处理
-% 由于parfor里不能进行内层并行，这里创建一个扁平化的索引，然后对这个扁平化的循环使用 parfor
-flatIndices = [];
-for i = 1:nsite
-    for j = 1:nsub
-        flatIndices = [flatIndices; i, j];
-    end
-end
-
-parfor idx = 1:size(flatIndices, 1)
-
-    i = flatIndices(idx, 1); % 站点索引
-    j = flatIndices(idx, 2); % 被试索引
-
-    try
-        siteFolderName = siteFolders(i).name;
-        siteLetter = siteFolderName(end);
-        siteFolderPath = fullfile(dataFolder, siteFolderName);
-        
-        subFolderName = subs{j};
-        subfolderPath = fullfile(siteFolderPath, subFolderName);
-                
-        if exist(subfolderPath, 'dir')
-            niiFilePaths = {};
-            runFolders = dir(fullfile(subfolderPath, '*PA_run*'));
-
-            for k = 1:length(runFolders)
-                runFolderName = runFolders(k).name;
-                runFolderPath = fullfile(subfolderPath, runFolderName);
-                niiFile = dir(fullfile(runFolderPath, '*.dtseries.nii'));
-
-                if isempty(niiFile)
-                    error('This sub doesnt have this data. Please check!');
-                end
-
-                niiFilePaths{end+1} = fullfile(runFolderPath, niiFile(1).name);
-            end
-
-            if ~isempty(niiFilePaths)
-                txtFileName = fullfile(outputFolder, sprintf('%s_site-%s.txt', subFolderName, siteLetter));
-                fileID = fopen(txtFileName, 'w');
-                for n = 1:length(niiFilePaths)
-                    if n == length(niiFilePaths)
-                        fprintf(fileID, '%s', niiFilePaths{n});
-                    else
-                        fprintf(fileID, '%s ', niiFilePaths{n});
-                    end
-                end
-                fclose(fileID);
-            end
-        end
-    catch ME
-        fprintf('Error processing subject %s in site %s: %s\n', siteFolderName, ME.message);
-    end
-end
-
-%---- 单机处理
+%---- 单机处理使用下面这部分代码
 for i = 1:nsite
 %     i = 1
     siteFolderName = siteFolders(i).name;
@@ -144,18 +143,14 @@ for i = 1:nsite
 %         j = 1
         subFolderName = subs{j};
         subfolderPath = fullfile(siteFolderPath, subFolderName);
-        
+
         if exist(subfolderPath, 'dir')
             niiFilePaths = {};
-
-%             runFolders = dir(fullfile(subfolderPath, 'MNINonLinear', 'Results', '*PA_run*'));
             runFolders = dir(fullfile(subfolderPath, '*PA_run*'));
 
             for k = 1:length(runFolders)
 %                 k = 1
                 runFolderName = runFolders(k).name;
-%                 runFolderPath = fullfile(subfolderPath, 'MNINonLinear', 'Results', ...
-%                     runFolderName);
                 runFolderPath = fullfile(subfolderPath, runFolderName);
                 niiFile = dir(fullfile(runFolderPath, '*.dtseries.nii'));
                 
@@ -186,81 +181,85 @@ end
 disp('Step 1 DONE!');
 
 %% Step 2 Generating profiles and initialization parameters
+% 生成功能连接矩阵，由于多回波ICA融合时会进行降噪，在step0里需要小心设置阈值
 % modified from CBIG_ComputeCorrelationProfile.m
-% 由于多回波ICA融合时会进行降噪，在step0里需要小心设置阈值
 
 out_dir = fullfile(project_dir, 'output', 'generate_profiles_and_ini_params');
 
-flatIndices = [];
+% %---- 并行处理使用下面这部分代码
+% flatIndices = [];
+% for j = 1:nsub
+%     for i = 1:nsite
+%         flatIndices = [flatIndices; j, i];
+%     end
+% end
+% 
+% parfor idx = 1:size(flatIndices, 1)
+% 
+%     j = flatIndices(idx, 1); % 被试索引
+%     i = flatIndices(idx, 2); % 站点索引
+% 
+%     fMRI_list = fullfile(out_dir,'data_list','fMRI_list',[subs{j} '_site-' site{i} '.txt']);
+%     
+%     if exist(fMRI_list, 'file')
+% 
+%         for t = 1:xthe 
+%             try
+%                 out_profile_dir = fullfile(out_dir,'profiles', subs{j}, ['site-' site{i}], ['t-' num2str(threshold(t))]);
+%         
+%                 if ~exist(out_profile_dir, 'dir')
+%                     mkdir(out_profile_dir);
+%                 end
+%         
+% %                 profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_' targ_mesh '_roi' seed_mesh '.surf2surf_profile.mat']);
+%                 profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_t-' num2str(threshold(t)) '.mat']);
+% 
+%                 TravelClub_CBIG_ComputeCorrelationProfile(seed_mesh, targ_mesh, profile_file, 'NONE', num2str(threshold(t)), ...
+%                    fMRI_list, 'NONE', 'NONE', '0');
+%                 %disp(['Threshold ', num2str(threshold(t)), ' ', subs{j} ' site-' site{i} ' DONE!' ]);
+%                 
+%             catch ME
+%                 fprintf('Error processing subject %s in site %s: %s\n', subs{j}, site{i}, ME.message);
+%             end
+%         end
+%     else
+%         disp([subs{j} ' site-' site{i} ' DOES NOT EXIT' ]);
+%     end
+% end
+
+%---- 单机处理使用下面这部分代码
 for j = 1:nsub
     for i = 1:nsite
-        flatIndices = [flatIndices; j, i];
-    end
-end
+%      i = 1;j = 1;
 
-parfor idx = 1:size(flatIndices, 1)
-
-    j = flatIndices(idx, 1); % 被试索引
-    i = flatIndices(idx, 2); % 站点索引
-
-    fMRI_list = fullfile(out_dir,'data_list','fMRI_list',[subs{j} '_site-' site{i} '.txt']);
-    
-    if exist(fMRI_list, 'file')
-
-        for t = 1:xthe 
-            try
+        fMRI_list = fullfile(out_dir,'data_list','fMRI_list',[subs{j} '_site-' site{i} '.txt']);
+        
+        if exist(fMRI_list, 'file')
+   
+            for t = 1:xthe 
+%                 t = 1;
                 out_profile_dir = fullfile(out_dir,'profiles', subs{j}, ['site-' site{i}], ['t-' num2str(threshold(t))]);
         
                 if ~exist(out_profile_dir, 'dir')
                     mkdir(out_profile_dir);
                 end
         
-%                 profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_' targ_mesh '_roi' seed_mesh '.surf2surf_profile.mat']);
                 profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_t-' num2str(threshold(t)) '.mat']);
 
                 TravelClub_CBIG_ComputeCorrelationProfile(seed_mesh, targ_mesh, profile_file, 'NONE', num2str(threshold(t)), ...
                    fMRI_list, 'NONE', 'NONE', '0');
-                %disp(['Threshold ', num2str(threshold(t)), ' ', subs{j} ' site-' site{i} ' DONE!' ]);
-                
-            catch ME
-                fprintf('Error processing subject %s in site %s: %s\n', subs{j}, site{i}, ME.message);
             end
+            
+        else
+            disp([subs{j} ' site-' site{i} ' DOES NOT EXIT' ]);
         end
-    else
-        disp([subs{j} ' site-' site{i} ' DOES NOT EXIT' ]);
     end
 end
-
-
-% for j = 1:nsub
-%  for i = 1:nsite
-% %      i = 1
-% %      j = 1
-% 
-%     out_profile_dir = fullfile(out_dir,'profiles', subs{j}, ['site-' site{i}]);
-% 
-%     if ~exist(out_profile_dir)
-%         mkdir(out_profile_dir);
-%     end
-% 
-%     profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_' targ_mesh ...
-%         '_roi' seed_mesh '.surf2surf_profile.mat']);
-%     fMRI_list = fullfile(out_dir,'data_list','fMRI_list',[subs{j} '_site-' site{i} '.txt']);
-% 
-%     if exist(fMRI_list)
-%         TravelClub_CBIG_ComputeCorrelationProfile(seed_mesh, targ_mesh, profile_file, 'NONE', num2str(threshold), ...
-%             fMRI_list, 'NONE', 'NONE', '0');
-%         disp([subs{j} ' site-' site{i} ' DONE!' ]);
-%     else
-%         disp([subs{j} ' site-' site{i} ' DOES NOT EXIT' ]);
-%     end
-%  end
-% end
-
 
 disp('Step 2 DONE!');
 
 %% Step 3 To obtain the group averaged profiles
+% 计算组水平功能连接，这部分处理简单不需要并行了
 % modified from CBIG_MSHBM_avg_profiles.m
 
 out_dir = fullfile(project_dir, 'output/generate_profiles_and_ini_params');
@@ -284,8 +283,6 @@ for t = 1:xthe
     
             out_profile_dir = fullfile(out_dir, 'profiles', subs{j}, ['site-', site{i}], ['t-', t_str]);  
 
-%             profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_' targ_mesh ...
-%                 '_roi' seed_mesh '.surf2surf_profile.mat']);
             profile_file = fullfile(out_profile_dir, [subs{j} '_site-' site{i} '_t-' t_str '.mat']);
         
             if exist(profile_file)
@@ -348,7 +345,6 @@ for n = 1:xnet
         
         output_file = fullfile(gro_dir, ['t-' t_str '_n-' n_str '_group.mat']);
 
-        % avg_profile_file = fullfile(out_dir,'profiles','avg_profile',[targ_mesh '_roi' seed_mesh '_avg_profile.mat']);
         avg_profile_file = fullfile(avg_profile_dir, ['t-' t_str '_avg_profile.mat']);
         
         CBIG_VonmisesSeriesClustering_fix_bessel_randnum_bsxfun(targ_mesh, '', networks(n), output_file, ...
